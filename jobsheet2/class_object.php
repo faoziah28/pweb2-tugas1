dcb0097fc42e7171f59d51622e54b2a113169b63
faoziah28 <152271367+faoziah28@users.noreply.github.com>
2024-09-18 10:27:57 +0700

    Update class_object.php

--- jobsheet2/class_object.php
+++ jobsheet2/class_object.php
@@ -3,23 +3,23 @@
 class Mahasiswa {
     /*atribut atau properties berfungsi menyimpan
      data atau keadaan dari objek*/
-    public $nama; //aksesbilitas PUBLIC bisa diakses dari mana saja
+    public $nama; //akses public bisa diakses semua kelas
     public $nim; 
     public $jurusan; 
-    //construct berfunsi Menginisialisasi atribut atau preoperty nama, nim, jurusan
+    //inisialisasi atribut atau preoperty nama, nim, jurusan
     public function __construct($nama, $nim, $jurusan){
         $this->nama = $nama;
         $this->nim = $nim;
         $this->jurusan = $jurusan;
     }
 
-    //metode atau function agar bisa tampilData
+    //metode atau function agar bisatampilData
     public function tampilData(){
         return "Nama: $this->nama <br> NIM: $this->nim <br> Jurusan: $this->jurusan";
     }
 }
 //instansiasi objek mahasiswa1
-$mahasiswa1 = new Mahasiswa("Anisa", "230302075", "komputer dan bisnis");
+$mhs = new Mahasiswa("Anisa", "230302075", "komputer dan bisnis");
 //menampilkan informasi tentang objek 
-echo $mahasiswa1->tampilData(); //output : Nama: Anisa, NIM: 230302075, Jurusan: komputer dan bisnis
+echo $mhs->tampilData(); //output : Nama: Anisa, NIM: 230302075, Jurusan: komputer dan bisnis
 ?>
